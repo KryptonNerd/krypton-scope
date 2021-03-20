53a593bbfd2a64ab84c3b6cf2f2c41b28dfab64c
KryptonNerd <rob_jess@outlook.com>
2021-03-20 23:17:39 +0000

    Update krypton-scope-main.py

--- software/krypton-scope-main.py
+++ software/krypton-scope-main.py
@@ -1,1 +1,20 @@
-#krypton-scope-main
+#krypton-scope-main.py
+
+import PySimpleGUI as sg
+
+layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
+
+# Create the window
+window = sg.Window("Demo", layout)
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
+
+#sg.Window(title="krypton-scope", layout=[[]], margins=(100, 50)).read()
